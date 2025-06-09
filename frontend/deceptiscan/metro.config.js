8b0209be6caa625d975dea8f6e321e3316458c3a
Khoo Jing Xiang <122257205+meowwtama@users.noreply.github.com>
2025-06-09 20:10:37 +0800

    Downgrade to SDK 52

--- frontend/deceptiscan/metro.config.js
+++ frontend/deceptiscan/metro.config.js
@@ -1,7 +1,8 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-/** @type {import('expo/metro-config').MetroConfig} */
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.unstable_enablePackageExports = false;
+
 module.exports = defaultConfig;
